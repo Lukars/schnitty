688b0f3014ab517b998cb046c7db413003f73f03
Lukars <lukas.siebeck@gmx.de>
2015-08-18 12:25:52 +0200

    cleaned up script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var  x = 0, y = 0,
     vx = 0, vy = 0,
 	ax = 0, ay = 0,
 	browserWidth = 1024,
-	secondPlayer=false,
 	os=getOS();
 
 socket.on('width', function(width){
@@ -13,38 +12,31 @@ socket.on('width', function(width){
 function getOS() {
   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-  if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
+  if( userAgent.match( /iPad/i ) 	|| 
+	  userAgent.match( /iPhone/i ) 	|| 
+  	  userAgent.match( /iPod/i ) ) 
   {
-    return 'iOS';
+	  return 'iOS';
+  } else if( 
+  	  userAgent.match( /Android/i ) )  {
+	  return 'Android';
+  } else {
+      return 'unknown';
   }
-  else if( userAgent.match( /Android/i ) )
-  {
-    return 'Android';
-  }
-  else
-  {
-    return 'unknown';
-  }
-}
+};
 
 if (window.DeviceMotionEvent != undefined) {
 	window.ondevicemotion = function(e) {
 		ax = event.accelerationIncludingGravity.x * 35;
-		//ay = event.accelerationIncludingGravity.y * 35;	
-	}
+	};
 
 	setInterval( function() {
-		var landscapeOrientation = window.innerWidth/window.innerHeight > 1;
 		if ( os === 'Android') {
 			vx = vx - ax;
-			//vy = vy + ax;
 		} else {
-			//vy = vy - ay;
 			vx = vx + ax;
 		}
 		vx = vx * 0.98;
-		//vy = vy * 0.98;
-		//y = parseInt(y + vy / 50);
 		x = parseInt(x + vx / 50);
 		
 		boundingBoxCheck();
@@ -56,7 +48,5 @@ if (window.DeviceMotionEvent != undefined) {
 
 function boundingBoxCheck () {
 	if (x<150) { x = 150; vx = 0; }
-	//if (y<0) { y = 0; vy = 0; }
 	if (x>browserWidth-150) { x = browserWidth-150; vx = 0; ax = 0; }
-	//if (y>browserHeight-200) { y = browserHeight-200; vy = 0; vy = 0; }
 };
